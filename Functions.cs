e9d4706e4fbf480ee103963d6b71d1f0ac0f626e
Vu Van Duy <vuvanduy12a5lx@gmail.com>
2020-04-12 20:22:54 +0700

    first commit

---
+++ Functions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nhan_vien
+{
+    class Functions
+    {
+    }
+}
---
+++ Funtions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace nhan_vien
+{
+    class Funtions
+    {
+        class Functions
+        {
+            public static SqlConnection Conn;  //Khai báo đối tượng kết nối
+            public static string connString;   //Khai báo biến chứa chuỗi kết nối
+
+            public static void Connect()
+            {
+                //Thiết lập giá trị cho chuỗi kết nối
+                connString = "Data Source=DESKTOP-6TI18BN\\SQLEXPRESS;Initial Catalog=quanlythuesach;Integrated Security=True";
+                Conn = new SqlConnection();                 //Cấp phát đối tượng
+                Conn.ConnectionString = connString;         //Kết nối
+                Conn.Open();                                //Mở kết nối
+            }
+
+            public static void Disconnect()
+            {
+                if (Conn.State == ConnectionState.Open)
+                {
+                    Conn.Close();       //Đóng kết nối
+                    Conn.Dispose();     //Giải phóng tài nguyên
+                    Conn = null;
+                }
+            }
+        }
+
+
+    }
+}
